4e0f9d57d2a5e7fe687837a4a4ca28334c56d516
rehan-bhatia <63210052+rehan-bhatia@users.noreply.github.com>
2020-04-28 20:22:00 +0530

    Updates

---
+++ precache-manifest.6c5543b7e4c20e38ec65ddd69912a971.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db194c26eb2ab53fe465c4695ef18e4",
+    "url": "/anbowe/index.html"
+  },
+  {
+    "revision": "a4b1838045a3bf39e776",
+    "url": "/anbowe/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "c8a89245f6cfac206c2f",
+    "url": "/anbowe/static/css/main.cd8bb35b.chunk.css"
+  },
+  {
+    "revision": "a4b1838045a3bf39e776",
+    "url": "/anbowe/static/js/2.f1a91ba8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anbowe/static/js/2.f1a91ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a89245f6cfac206c2f",
+    "url": "/anbowe/static/js/main.92cb579e.chunk.js"
+  },
+  {
+    "revision": "fe45b18005a0de58757d",
+    "url": "/anbowe/static/js/runtime-main.9523d7bf.js"
+  }
+]);
